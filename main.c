63da64693012605753b1c7e7605cca2d994f9653
Jackson Kidwell <130793828+GingerTron1234@users.noreply.github.com>
2023-10-15 16:45:15 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,71 +1,12 @@
 #include <stdio.h>
 
-int main() {
+void printAmount(unsigned long wc, unsigned long cc, unsigned long tc, unsigned long nc, unsigned long sc, long nwcount[], char pcount[]) {
 
-    int c;
+printf("\nWhat you wrote but spell corrected:\n-----------------------------------------\n\n");
+printf("%s\n", pcount);
+printf("-----------------------------------------");
 
-    long cc = 0; //character count
-    long nc = 0; //new line count
-    long tc = 0; //tab count
-    long sc = 0; //space count
-    long wc = 0; //word count
-    short wl = 0;//temporary word length
-    long nwcount[10];//word length count
-    for (int i; i < 10; i++)
-        nwcount[i] = 0; 
-
-    #define INWORD 1
-    #define OUTWORD 0
-    short state  = OUTWORD;
-
-    short blankCheck = 0;
-
-    printf("Welcome to Jackson's Word Helper!\n");
-    printf("Enter Ctrl + Z into the terminal to end.\n");
-    printf("-----------------------------------------\n");
-    printf("\n");
-
-    while((c = getchar()) != EOF) {
-
-    
-        if(c == ' ') {
-            
-            if(blankCheck == 0) {
-
-                blankCheck = 1;
-                sc++;
-                putchar(c);
-            }
-                
-        } else {
-            blankCheck = 0;
-            putchar(c);
-        }
-
-        if(c == '\n') {
-            nc++;
-        }
-        
-        if(c == '\t') {
-            tc++;
-        }    
-        
-        if(c == '\n' || c == '\t' || c == ' ') {
-            state = OUTWORD;
-            nwcount[wl]++;
-            wl = 0;
-        } else if (state == OUTWORD) {
-            wc++;
-            cc++;
-            wl++;
-            state = INWORD;
-        } else {
-            cc++;
-            wl++;
-        }
-    }
-
-    printf("\n%5d words.\n", wc);
+printf("\n%5d words.\n", wc);
     printf("%5d characters.\n", cc);
     printf("%5d tabs.\n", tc);
     printf("%5d new lines.\n", nc);
@@ -80,10 +21,81 @@ int main() {
             printf(" %d", nwcount[i]);
         }
     }
+}
+
+int main() {
+
+    char c;
+
+    unsigned long cc = 0; //character count
+    unsigned long nc = 0; //new line count
+    unsigned long tc = 0; //tab count
+    unsigned long sc = 0; //space count
+    unsigned long wc = 0; //word count
+    unsigned short wl = 0;//temporary word length
+    char pcount[99999];//whole paragraph count.
+    unsigned long pc = 0; //how long the paragraph is.
+    long nwcount[10];//word length count
+
+    #define INWORD 1
+    #define OUTWORD 0
+    short state  = OUTWORD;
+
+    short blankCheck = 0;
+
+    printf("Welcome to Jackson's Word Helper! (Beta 1.1.2)\n");
+    printf("Enter Ctrl + Z into the terminal to end.\n");
+    printf("-----------------------------------------\n\n");
+
+    while((c = getchar()) != EOF) {
+
     
+        if(c == ' ') {
+            
+            if(blankCheck == 0) {
+
+                blankCheck = 1;
+                sc++;
+                pcount[pc] = c;
+                pc++;
+            }
+                
+        } else {
+            blankCheck = 0;
+            pcount[pc] = c;
+            pc++;
+        }
+
+        if(c == '\n') {
+            nc++;
+        }
+        
+        if(c == '\t') {
+            tc++;
+        }    
+        
+        if(c == '\n' || c == '\t' || c == ' ' || c == '.' || c == '?' || c == '!' || c == ',') { //Sees if there's a seperation between differen't words.
+            state = OUTWORD;
+            nwcount[wl]++;
+            wl = 0;
+        } else if (state == OUTWORD) {
+            wc++;
+            cc++;
+            wl++;
+            state = INWORD;
+        } else {
+            cc++;
+            if(c != '\'')
+                wl++;
+        }
+    }
+
+    
+    printAmount(wc, cc, tc, nc, sc, nwcount, pcount);
 
     printf("\nPress Enter to Exit:");
     scanf("Exit Check");
 
     return 0;
 }
+
